ef8141d8cdf77ac1c7755905a9e5131e0a12de01
CIBI.P _GCEB <brohari740@gmail.com>
2022-04-16 11:21:34 +0530

    upload

---
+++ grading/home.php
@@ -0,0 +1,40 @@
+<style>
+            .heading{
+                background-image: url('https://github.com/broharigunda/HOsting_SEarchedUpdates/blob/master/school.png?raw=true');
+                background-size:cover;
+                background-repeat: no-repeat;
+                background-position:center center;
+                border-radius:20px;
+                box-shadow:1px 1px 10px 1px black;
+                padding:20px;
+            }
+            span{
+                margin-left:30px;
+                color:white;
+                font-weight:bolder;
+            }
+            .imgg{
+                width:100%;
+                height:50%;
+            }
+            .page-header img{
+                width:100px;
+                height:100px;
+            }
+            @media (max-width:400px){
+                span{
+                    font-size:24px;
+                }
+                .page-header img{
+                 width:50px;
+                 height:50px;                                                                                                                                                               
+            }
+            }
+            
+            </style>
+            <div class="heading">
+          <h1 class="page-header"><img src="https://github.com/broharigunda/HOsting_SEarchedUpdates/blob/master/Icon-Logo6.png?raw=true" style="height:150px; width:150px;">ICON School Management System</h1>
+        </div>
+          <img class="imgg" src="https://github.com/broharigunda/HOsting_SEarchedUpdates/blob/master/520.jpg?raw=true">
+          
+			
